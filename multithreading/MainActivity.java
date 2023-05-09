4ecf7e01c886d73de4c554bd2507548dc209de3b
niceguynix <its200manoj@gmail.com>
2023-05-09 07:23:28 +0530

    .

--- multithreading/MainActivity.java
+++ multithreading/MainActivity.java
@@ -15,25 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         timer=(TextView) findViewById(R.id.tv2);
         clickme=(Button) findViewById(R.id.button);
-        hand.postDelayed(run,1000);
+        hand.postDelayed(this::updateTimer,1000);
     }
-    Runnable run=new Runnable() {
-        @Override
-        public void run() {
-            updateTimer();
-        }
-    };
+
     public void updateTimer() {
         timer.setText("" + (Integer.parseInt(timer.getText().toString()) - 1));
         if (Integer.parseInt(timer.getText().toString()) == 0) {
             clickme.setVisibility(View.VISIBLE);
         } else {
-            hand.postDelayed(run, 1000);
+            hand.postDelayed(this::updateTimer, 1000);
         }
     }
-    public void clicker(View view) {
-        hand.postDelayed(run, 1000);
-        timer.setText("10");
-        clickme.setVisibility(View.INVISIBLE);
-    }
+    
 }
